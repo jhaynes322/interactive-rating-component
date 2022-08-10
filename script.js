c726b48aa53f08c0c804d7a18224b0b7cebe2f1b
Jared Haynes <jared.haynes@charter.net>
2022-08-10 17:59:59 -0500

    Wrote out algorithm to plan my code.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//setting up dev environment for project
+//User clicks one of five rating buttons:
+//  +JS should: *store # as variable (ratingNum);
+//              *change color of button clicked;
+
+//When user clicks 'submit':
+//  +JS should: *open new card;
+//              *concatenate obj. literal to string to make rating statement;
+//              *append rating statement to DOM;
+
+//First, store clicked rating num as variable,
+//then, make a function that creates rating string.
+//That function should return a new variable with the full string.
+//Finally, append that new variable to the HTML (new card)
