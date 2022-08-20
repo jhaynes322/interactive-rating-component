74ae67f8d3107f1b37d2ce6d9d6db64575047e99
Jared Haynes <jared.haynes@charter.net>
2022-08-20 15:55:49 -0500

    Fixed adding rating to new card string.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const oldCard = document.querySelector('.oldCard');
 const buttons = document.querySelectorAll('button');
 const submitBtn = document.querySelector('.submit');
 const newCard = document.querySelector('.thanksContainer');
-const rateChoice = document.getElementById('#selectedRating');
+const rateChoice = document.querySelector('#selectedRating');
 let rate = 0;
 
 buttons.forEach(num => {
@@ -25,7 +25,7 @@ function rateHandler() {
         alert("Please select a rating")
     } else {
         //need to try to append rate to rating-string
-        console.log(rate);
+        rateChoice.innerHTML = rate
         newCard.classList.remove('hidden');
         oldCard.classList.add('hidden');
     }
